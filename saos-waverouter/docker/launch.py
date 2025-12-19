1fccb0305cd5173ee3308b5036aa0939fb38607a
Yvan Niyonzima <yniyonzi@ciena.com>
2025-12-18 22:27:15 -0500

    Added OSPF IBox to containerlab sim
    Reason: WRSW-50458
    
    Implement WR_ib class and added wr-ibox to vm_class list in launch.py
    
    Testing
    
    - Deployed a Waverouter topology  with a CTM and IBox.
    - Used gnmic command to confiure an OSFP card type.
    - show system components showed OB12-16xOSFP800 as the Configured-Type in the IBox slot.
    - Attached are the json and yaml file used to deploy the topology

--- saos-waverouter/docker/launch.py
+++ saos-waverouter/docker/launch.py
@@ -701,6 +701,125 @@ class WR_fb(WR_base):
 
         return res
 
+class WR_ib(WR_base):
+    def __init__(self, hostname, username, password, conn_mode, num, housing_id,
+                 location_id, nic_eth_start, ram_mb=None):
+        if ram_mb is None:
+            ram_mb = 5120
+
+        super(WR_ib, self).__init__(
+            hostname=hostname,
+            username=username,
+            password=password,
+            conn_mode=conn_mode,
+            num=num,
+            housing_id=housing_id,
+            location_id=location_id,
+            variant="wr-ibox",
+            product_number="ob1216xosfp800",
+            size=str(ram_mb * 1024 * 1024),
+            num_backplane_if=5,
+            ram=ram_mb,
+            num_nics=16,  # 16 instead of 15
+            smp="4,sockets=1,dies=1,cores=2,threads=2"
+        )
+        self.start_nic_eth_idx = nic_eth_start
+        self.num_fabric_if = 1
+        self.fabric_if = f"{self.name}fb0"
+
+    def gen_mgmt(self):
+        """Generate mgmt interface(s)
+
+        We override the default function for the wr-ib
+        """
+        res = []
+
+        # Generate hostfwd rules for deploy script integration
+        hostfwd_rules = self.generate_hostfwd_rules()
+
+        # Calculate DHCP IP for this VM
+        dhcp_ip = DEBUG_DHCP_START_BASE + self.num
+
+        # debug interface with hostfwd port forwarding
+        res.extend(["-device",
+                    "virtio-net-pci,netdev=p00,mac=%s,multifunction=on,addr=0x3" % self.mgmt_mac,
+                    "-netdev",
+                    f"user,id=p00,net={DEBUG_NETWORK_BASE}/{DEBUG_SUBNET_MASK},host={DEBUG_HOST_IP},dns={DEBUG_DNS_IP},dhcpstart={DEBUG_NETWORK_BASE_STR}.{dhcp_ip},{hostfwd_rules}"])
+
+        # add virtio NIC for internal control plane interface to wr-qb
+        res.extend(["-device",
+                    "virtio-net-pci,netdev=%s,mac=%s,bus=pcie.0,multifunction=on,addr=0x4"
+                    % (self.if_list[0], gen_shared_mac(int(self.housing_id), int(self.location_id), 2, 0)),
+                    "-netdev",
+                    "tap,ifname=%s,id=%s,script=no,downscript=no" % (self.if_list[0], self.if_list[0])])
+
+        for i in range(1, self.num_backplane_if):
+            res.extend(["-device",
+                        "virtio-net-pci,netdev=%s,mac=%s,bus=pcie.0,addr=0x4.0x%s"
+                        % (self.if_list[i], gen_shared_mac(int(self.housing_id),  int(self.location_id), i+2, i), i),
+                        "-netdev",
+                        "tap,ifname=%s,id=%s,script=no,downscript=no" % (self.if_list[i], self.if_list[i])])
+
+        # Fabric interface
+        if self.num_fabric_if == 1:
+            res.extend(["-device",
+                        "virtio-net-pci,netdev=%s,mac=%s,bus=pcie.0,multifunction=on,addr=0x4.0x5"
+                        % (self.fabric_if, gen_shared_mac(int(self.housing_id), int(self.location_id), 0xfb, 0x01)),
+                        "-netdev",
+                        "tap,ifname=%s,id=%s,script=no,downscript=no" % (self.fabric_if, self.fabric_if)])
+
+        return res
+
+    def gen_nics(self):
+        """Generate qemu args for the normal traffic carrying interface(s)"""
+        self.nic_provision_delay()
+
+        res = []
+
+        if self.conn_mode == "tc":
+            self.create_tc_tap_ifup()
+
+        start_eth = self.start_nic_eth_idx
+        end_eth = self.start_nic_eth_idx + self.num_nics
+        addr = 0x8
+        ext = 0x0
+        for i in range(start_eth, end_eth):
+
+            if ext == 0:
+                mf = "multifunction=on,"
+                ext_str = ""
+            else:
+                mf = ""
+                ext_str = f".0x{ext}"
+
+            mac = f"{self.mgmt_mac[:-2]}{(i+0x10):02x}"
+
+            res.extend([
+                "-device",
+                f"{self.nic_type},netdev=p{i:02d},mac={mac},bus=pcie.0,{mf}addr=0x{addr:02x}{ext_str}"
+            ])
+
+            # if the matching container interface ethX doesn't exist, create a dummy interface
+            if not os.path.exists(f"/sys/class/net/eth{i}"):
+                res.extend(["-netdev", f"socket,id=p{i:02d},listen=:{i + 10000}"])
+            else:
+                res.extend(["-netdev", f"tap,id=p{i:02d},ifname=tap{i},script=/etc/tc-tap-ifup,downscript=no"])
+
+            # increment bus addressing
+            ext = (ext + 1) % 8
+            if ext == 0:
+                addr += 1
+
+        return res
+
+    def start(self):
+        super(WR_ib, self).start()
+
+        # Add interface to fabric bridge
+        self.logger.info(f"Adding fabric interface from {self.name} to the fabric bridge...")
+        if self.num_fabric_if:
+            vrnetlab.run_command(["brctl", "addif", f"{FABRIC_BRIDGE}", f"{self.fabric_if}"])
+            vrnetlab.run_command(["ip", "link", "set", self.fabric_if, "up"])
 
 class WR(vrnetlab.VR):
     def __init__(self, hostname, username, password, conn_mode):
@@ -739,6 +858,7 @@ class WR(vrnetlab.VR):
             "wr-ctm": WR_ctm,
             "wr-qbox": WR_qb,
             "wr-fbox": WR_fb,
+            "wr-ibox": WR_ib
         }
 
         self.logger.debug(f"Number of nodes: {len(vm_info)}")
@@ -788,7 +908,7 @@ class WR(vrnetlab.VR):
                         location_id
                     ]
 
-                    if box_type == "wr-qbox":
+                    if box_type in ["wr-qbox", "wr-ibox"]:
                         args.append(start_eth)
                         vm = vm_class[box_type](*args, **kwargs)
                         start_eth += vm.num_nics
