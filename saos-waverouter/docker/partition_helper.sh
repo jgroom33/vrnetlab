228c606a216ee0341a7970e7e5c380c892c1bd6b
Jeff Groom <jgroom@ciena.com>
2026-02-21 09:07:24 -0700

    cleanup: remove ciena waverouter code

--- saos-waverouter/docker/partition_helper.sh
+++
@@ -1,8 +0,0 @@
-FILTER='^loop'
-lsblk --raw -a --output "NAME,MAJ:MIN" --noheadings | grep -E "$FILTER" | while read LINE; do
-    DEV=/dev/$(echo $LINE | cut -d' ' -f1)
-    MAJMIN=$(echo $LINE | cut -d' ' -f2)
-    MAJ=$(echo $MAJMIN | cut -d: -f1)
-    MIN=$(echo $MAJMIN | cut -d: -f2)
-    [ -b "$DEV" ] || mknod "$DEV" b $MAJ $MIN
-done
